2c5d46fce58b4db12cdba76a9a7569c365b9f196
Jay Wineinger <jay.wineinger@gmail.com>
2014-01-05 22:01:33 -0600

    Just a little cleanup

--- htu21d.py
+++ htu21d.py
@@ -3,6 +3,9 @@ import time
 import quick2wire.i2c as i2c
 import binascii
 
+
+class CRCFailed(Exception): pass
+
 class HTU21D:
 
     CMD_READ_TEMP_HOLD = 0xe3
@@ -13,15 +16,16 @@ class HTU21D:
     CMD_READ_USER_REG = 0xe7
     CMD_SOFT_RESET= 0xfe
 
+    # all HTU21Ds are hardcoded to this address.
     ADDR = 0x40
 
-    # uses bits 7 and 0 of user_register as a 2-bit value mapping
+    # uses bits 7 and 0 of the user_register mapping
     # to the bit resolutions of (relative humidity, temperature)
     RESOLUTIONS = {
-        0 : (12, 14),
-        1 : (8, 12),
-        2 : (10, 13),
-        3 : (11, 11),
+        (0, 0) : (12, 14),
+        (0, 1) : (8, 12),
+        (1, 0) : (10, 13),
+        (1, 1) : (11, 11),
     }
 
     # sets up the times to wait for measurements to be completed. uses the
@@ -33,14 +37,17 @@ class HTU21D:
         (11, 11): (.01, .009),
     }
 
-    def __init__(self):
+    def __init__(self, debug=False):
         self.bus = i2c.I2CMaster()
         self.resolutions = self.get_resolutions()
         self.rh_timing, self.temp_timing = self.MEASURE_TIMES[self.resolutions]
+        self.debug = debug
         
-    def check_crc(self, sensor_val, debug=False):
+    def check_crc(self, sensor_val):
         """
-        Give this function the 2 byte message (measurement) and the check_value byte from the HTU21D
+        Adapted from https://github.com/sparkfun/HTU21D_Breakout/blob/master/Library/HTU21D_Humidity/HTU21D.cpp
+        
+        Give this function the 3 byte reading from the HTU21D
         If it returns 0, then the transmission was good
         If it returns something other than 0, then the communication was corrupted
         From: http://www.nongnu.org/avr-libc/user-manual/group__util__crc.html
@@ -61,7 +68,7 @@ class HTU21D:
 
         # Operate on only 16 positions of max 24. The remaining 8 are our remainder and should be zero when we're done.
         for i in range(16):
-            if debug:
+            if self.debug:
                 print("remainder: {0:024b}".format(remainder));
                 print("divisor:   {0:024b}".format(divisor));
 
@@ -71,7 +78,7 @@ class HTU21D:
             divisor >>= 1 # Rotate the divisor max 16 times so that we have 8 bits left of a remainder
 
         if remainder:
-            raise Exception("CRC failed. Remainder of {}".format(remainder))
+            raise CRCFailed("CRC checksum failed.")
 
     def reset(self):
         self.bus.transaction(i2c.writing_bytes(self.ADDR, self.CMD_SOFT_RESET))
@@ -83,7 +90,7 @@ class HTU21D:
             i2c.reading(self.ADDR, 1),
         )
         user_reg_int = int.from_bytes(user_reg[0], byteorder="big")
-        return self.RESOLUTIONS[(user_reg_int >> 6) | (user_reg_int & 0x1)]
+        return self.RESOLUTIONS[user_reg_int >> 6, user_reg_int & 0x1]
         
     def get_temp(self):
         self.bus.transaction(i2c.writing_bytes(self.ADDR, self.CMD_READ_TEMP_NOHOLD))
@@ -91,13 +98,13 @@ class HTU21D:
         results = self.bus.transaction(i2c.reading(self.ADDR, 3))
 
         raw_reading = results[0]
-        #print(binascii.hexlify(raw_reading))
+        if self.debug:
+            print(binascii.hexlify(raw_reading))
 
         raw_temp = int.from_bytes(raw_reading, byteorder="big")
         self.check_crc(raw_temp)
         raw_temp = (raw_temp & 0xFFFC00) >> 8
-        temp = -46.85 + (175.72 * (raw_temp / float(2**16)))
-        return temp
+        return -46.85 + (175.72 * (raw_temp / float(2**16)))
 
     def get_rel_humidity(self):
         self.bus.transaction(i2c.writing_bytes(self.ADDR, self.CMD_READ_HUM_NOHOLD))
@@ -105,13 +112,13 @@ class HTU21D:
         results = self.bus.transaction(i2c.reading(self.ADDR, 3))
 
         raw_reading = results[0]
-        #print(binascii.hexlify(raw_reading))
+        if self.debug:
+            print(binascii.hexlify(raw_reading))
 
         raw_hum = int.from_bytes(raw_reading, byteorder="big")
         self.check_crc(raw_hum)
         raw_hum = (raw_hum & 0xFFFC00) >> 8
-        temp = -6 + (125 * (raw_hum / float(2**16)))
-        return temp
+        return -6 + (125 * (raw_hum / float(2**16)))
 
 if __name__ == '__main__':
     sensor = HTU21D()
